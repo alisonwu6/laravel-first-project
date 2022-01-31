b7e3526ce7d29c72d1b1bf7f70515f3a16562d32
Alison Wu <alison.wu23@gmail.com>
2022-01-31 12:04:57 +0800

    Query Builder

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,10 +3,69 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostsController extends Controller
 {
     public function index() {
-        return view('posts/index');
+
+
+        // $posts = DB::select('select * from posts WHERE id = ?', [3]);
+        // $posts = DB::select('select * from posts WHERE id = :id', ['id' => 3]);
+
+
+        // chain methods together to build a query
+        $id = 3;
+        // $posts = DB::table('posts')
+        //     ->where('id', $id)
+        //     ->get();
+
+        // $posts = DB::table('posts')
+        //     ->select('body')
+        //     ->get();
+
+        // $posts = DB::table('posts')
+        //     ->where('created_at', '>', now()->subDay())
+        //     ->orWhere('title', 'Prof.')
+        //     ->get();
+        
+        // $posts = DB::table('posts')
+        //     ->whereBetween('id', [1,2])
+        //     ->get();
+
+        // $posts = DB::table('posts')
+        //     ->whereNotNull('title')
+        //     ->get();
+
+        // $posts = DB::table('posts')
+        //     ->select('title')
+        //     ->distinct()
+        //     ->get();
+
+        // $posts = DB::table('posts')
+        //     ->orderBy('title', 'asc')
+        //     ->get();
+
+        // $posts = DB::table('posts')
+        //     // ->latest()  
+        //     // ->oldest()   
+        //     ->inRandomOrder()
+        //     ->get();
+
+        // $posts = DB::table('posts')
+            // ->insert(['title' => 'New Title', 'body' =>'New BOdy']);  // true
+
+        // $posts = DB::table('posts')
+        //     ->where('id', "=", 6)
+        //     -> update(['title' => 'Hello', 'body' => 'This is my day.']);   // 1   (true)
+
+        // $posts = DB::table('posts')
+        //     ->where('id', "=", 4)
+        //     -> delete();
+
+        $posts = DB::table('posts')
+            ->get();
+        
+        dd($posts);
     }
 }
